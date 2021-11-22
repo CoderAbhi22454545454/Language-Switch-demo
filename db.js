bf47c1cab803eeb0de8d900a71318a73512eafd2
Abhishek Mohalkar <90196689+CoderAbhi22454545454@users.noreply.github.com>
2021-11-22 22:46:03 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,15 @@
+var data = {
+			  "bosnia": 
+			  {
+			    "title": "Želiš da budeš viđen?",
+			    "description": 
+				    "	Svaki zadatak obavljen u našoj radionici, prilagođen je vašim potrebama Svaki klijent zahtijeva poseban pristup i detaljno sagledavanje svih parametara. Tu smo veoma ozbiljni i odgovorni. Konsultacije su besplatne."
+			  },
+			  "english": 
+			  {
+			    "title": "Want to be seen?",
+			    "description": 
+				 "Every task performed in our workshop is tailored to your needs. Each client requires a special approach and a detailed consideration of all parameters. We are very serious and responsible here. Consultations are free."
+			  },
+	
+			}
